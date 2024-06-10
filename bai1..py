000ebe4a327597a414591989fc6e991236e84c8a
Dao Manh Dung <daomanhdungtvt@gmail.com>
2024-06-10 11:41:18 +0700

    cc

---
+++ bai1..py
@@ -0,0 +1,26 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+n = int(input())
+s = Stack()
+m = list(map(int, input().split())
+k = 0
+for x in m:
+    if x > s.peek()
